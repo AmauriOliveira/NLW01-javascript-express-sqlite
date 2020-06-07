c9de7446e1b42fd0e8f48f2e35c5e2403ea73272
Amauri Antonio de Oliveira <amauriibate32@hotmail.com>
2020-06-07 15:14:00 -0300

    clean database.db

--- src/database/db.js
+++ src/database/db.js
@@ -2,7 +2,7 @@ const sqlite3 = require("sqlite3").verbose();
 
 const db = new sqlite3.Database("./src/database/database.db");
 module.exports = db;
-/*
+ /*
 db.serialize(() => {
     db.run(`
     CREATE TABLE IF NOT EXISTS places (
@@ -18,53 +18,54 @@ db.serialize(() => {
         items TEXT
     );
     `)
-    const query = `INSERT INTO places (
-                                    logo,
-                                    name,
-                                    phone,
-                                    email,
-                                    address,
-                                    address2,
-                                    state,
-                                    city,
-                                    items
-                                    ) VALUES (?,?,?,?,?,?,?,?,?);`
+});
+   
+const query = `INSERT INTO places (
+                                logo,
+                                name,
+                                phone,
+                                email,
+                                address,
+                                address2,
+                                state,
+                                city,
+                                items
+                                ) VALUES (?,?,?,?,?,?,?,?,?);`
 
-    const values = ["https://bhrecicla.com.br/imagens/blogpost69.jpg",
-        "Colectoria",
-        "1699188-99188",
-        "amauri@cregames.com",
-        "Odair marques  mariana",
-        "41",
-        "Sao Paulo",
-        "Ibate",
-        "Lâmpadas, Pilhas e Baterias"]
+const values = ["https://bhrecicla.com.br/imagens/blogpost69.jpg",
+    "Colectoria",
+    "1699188-99188",
+    "amauri@cregames.com",
+    "Odair marques  mariana",
+    "41",
+    "Sao Paulo",
+    "Ibate",
+    "Lâmpadas, Pilhas e Baterias"]
 
-    function afterInsertData(err) {
+function afterInsertData(err) {
+    if (err) {
+        return console.log(err);
+    }
+    console.log("Cadastrado com sucesso");
+    console.log(this);
+}
+
+db.run(query, values, afterInsertData)
+
+    db.all('SELECT * FROM PLACES', function (err, rows) {
         if (err) {
             return console.log(err);
         }
-        console.log("Cadastrado com sucesso");
-        console.log(this);
-    }
+        console.log("Aki esta os registros");
+        console.log('====================================');
+        console.log(rows);
+        console.log('====================================');
+    })
 
-    db.run(query, values, afterInsertData)
-
-        db.all('SELECT * FROM PLACES', function (err, rows) {
-            if (err) {
-                return console.log(err);
-            }
-            console.log("Aki esta os registros");
-            console.log('====================================');
-            console.log(rows);
-            console.log('====================================');
-        })
-
-        db.run('DELETE FROM places WHERE id = ?', [2],function(err){
-            if (err) {
-                return console.log(err);
-            }
-            console.log("apagado com sucesso");
-        })
-
-}); */
+    db.run('DELETE FROM places WHERE id = ?', [2],function(err){
+        if (err) {
+            return console.log(err);
+        }
+        console.log("apagado com sucesso");
+    })
+*/
